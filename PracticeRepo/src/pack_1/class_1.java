2e28ed738dc04936a2caf04db16cf91d67381e61
Mayur Ambekar <Mayur Ambekar@MayurJain>
2022-08-26 11:41:24 +0530

    first push

---
+++ PracticeRepo/src/pack_1/class_1.java
@@ -0,0 +1,22 @@
+package pack_1;
+
+public class class_1 
+{
+	
+	public static void sum()
+	{
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println(c);
+		
+	}
+	
+	
+	
+ public static void main(String[] ags)
+ {
+	 sum();
+ }
+
+}
